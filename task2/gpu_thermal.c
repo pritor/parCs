5fba1feecb3f4c19e1ad46ee72afef02e339eaf5
pritor <45505127+pritor@users.noreply.github.com>
2023-03-08 02:30:04 +0700

    some comments and deleting useless code

--- task2/gpu_thermal.c
+++ task2/gpu_thermal.c
@@ -17,20 +17,15 @@ int main(int argc, char** argv) {
 
     array[0] = 10.0;
     array[size-1] = 20.0;
-    array[(size-1) *size] = 20.0;
+    array[(size-1) *size] = 20.0;                                                 //угловые значения
     array[size * size-1] = 30.0;
-
-    arraynew[0] = 10.0;
-    arraynew[size - 1] = 20.0;
-    arraynew[(size - 1) * size] = 20.0;
-    arraynew[size * size - 1] = 30.0;
 
     double error = 1.0;
     double step = 10.0/(size-1);
     int realsize = size*size;
 #pragma acc parallel loop
     for (int i = 1; i < size; i++) {
-        array[i] = array[0] + step * i;
+        array[i] = array[0] + step * i;                                           //значения рамки
         array[size * (size - 1) + i] = array[(size - 1) * size] + step*i;
         array[(size * i)] = array[0] + step*i;
         array[size - 1 + i * size] = array[size - 1] + step * i;
@@ -43,12 +38,12 @@ int main(int argc, char** argv) {
     	clock_t begin = clock();
         for (; (k <= iternum) && (error > accuracy); k++) {
 	    if(k%100==0){
-		#pragma	acc kernels async(1)    
+		#pragma	acc kernels async(1)                                         //обнуление ошибки и перенос её на gpu каждые 100 итераций   
             	error =0;
 		#pragma acc update device(error) async(1)
 		}
 #pragma acc data present(array, arraynew, error)   
-#pragma acc parallel loop independent collapse(2) vector vector_length(256) gang num_gangs(128) reduction(max:error) async(1)	    
+#pragma acc parallel loop independent collapse(2) vector vector_length(256) gang num_gangs(128) reduction(max:error) async(1)	    //основной алгоритм
             for (int i = 1; i < size - 1; i++) {
                 for (int j = 1; j < size - 1; j++) {
                     arraynew[j + i * (size)] = 0.25 * (array[j + (i + 1) * (size)] + array[j + (i - 1) * (size)] + array[j - 1 + i * (size)] + array[j + 1 + i * (size)]);
@@ -57,7 +52,7 @@ int main(int argc, char** argv) {
 
             }
 	    if(k%100==0){
-		#pragma acc update host(error) async(1)
+		#pragma acc update host(error) async(1)                           //обновление ошибки на cpu
 		#pragma acc wait(1)    
 	    }
 	    double* temp = array;
